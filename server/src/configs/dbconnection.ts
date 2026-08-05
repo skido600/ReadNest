10e3b1d1b00de281dde0a9326d6d8c6eac778c49
Chuks <63956312+skido600@users.noreply.github.com>
2026-08-05 17:40:55 +0100

    bun

--- server/src/configs/dbconnection.ts
+++ server/src/configs/dbconnection.ts
@@ -1,11 +1,14 @@
 import { drizzle } from "drizzle-orm/neon-http";
-import { neon } from "@neondatabase/serverless";
 import { config } from "dotenv";
+import { neon } from "@neondatabase/serverless";
+import * as schema from "../models/schema.ts";
 config();
+
 const pgUrl = process.env.DATABASE_URL;
 if (!pgUrl) {
   throw new Error("DB_URL is not set in environment variables");
 }
 
-const sql = neon(pgUrl!);
-export const db = drizzle({ client: sql });
+const sql = neon(pgUrl);
+export const db = drizzle(sql, { schema });
+console.log("Connected to PostgreSQL via Drizzle");
