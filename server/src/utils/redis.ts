f3c31e9c59e9d823419282bb3f5b4e55eb011076
Chuks <63956312+skido600@users.noreply.github.com>
2026-08-05 18:08:07 +0100

    drizzle

--- server/src/utils/redis.ts
+++ server/src/utils/redis.ts
@@ -1,15 +1,16 @@
 import Redis from "ioredis";
 import { config } from "dotenv";
 config();
-
-// const client = new Redis(process.env.REDIS_URL as string, {
-//   maxRetriesPerRequest: null,
-// });
-const client = new Redis({
-  host: "127.0.0.1", // Redis server host
-  port: 6379, // Redis server port
-  maxRetriesPerRequest: null, // Disable retry limitation
+console.log(process.env.REDIS_URL, "redis local");
+const client = new Redis(process.env.REDIS_URL as string, {
+  maxRetriesPerRequest: null,
 });
+
+// const client = new Redis({
+//   host: "127.0.0.1", // Redis server host
+//   port: 6379, // Redis server port
+//   maxRetriesPerRequest: null, // Disable retry limitation
+// });
 client.on("connect", () => {
   console.log(" Redis connected successfully");
 });
