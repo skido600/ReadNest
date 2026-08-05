2a9fb426bb78120d25673fd3b99fa4e56473eeb5
Chuks <63956312+skido600@users.noreply.github.com>
2026-08-06 00:28:14 +0100

    all done

--- server/src/index.ts
+++ server/src/index.ts
@@ -17,7 +17,7 @@ const app = express();
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://read-nest-431c.vercel.app"],
     credentials: true,
   }),
 );
